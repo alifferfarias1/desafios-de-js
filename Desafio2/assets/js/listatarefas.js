0909f38f1041605e46553756ec369b31e89020c9
Aliffer Farias <alifferalvesfarias3@gmail.com>
2023-11-13 23:44:28 -0300

    pequenas mudanças

--- Desafio2/assets/js/listatarefas.js
+++ Desafio2/assets/js/listatarefas.js
@@ -1,81 +1,80 @@
 // primeiro vamos selecionar os elementos html que desejamos alterar/mexer
-function listOfTaks(){
-
-const addTarefa = document.querySelector('.tarefa')
 const inputTarefa = document.querySelector('.add-tarefa')
-const inputButton = document.querySelector('.button-add')
+const inputBtn = document.querySelector('.button-add')
+const addTarefas = document.querySelector('.tarefas')
 
-function criaLi(){
+
+function criaLi() {
     const li = document.createElement('li');
     return li;
 }
+
 inputTarefa.addEventListener('keypress', function (e) {
-    if (e.keyCode === 13){
+    if (e.keyCode === 13) {
         if (!inputTarefa.value) return;
         criaTarefa(inputTarefa.value);
     }
 });
 
-
-function limparInput(){
+function limparInput() {
     inputTarefa.value = '';
     inputTarefa.focus();
 }
 
-inputButton.addEventListener('click', function(e){
-    if (!inputTarefa.value) return;
-    criaTarefa(inputTarefa.value)
-})
-
-function criaTarefa(textoInput){
+function criaTarefa(textoInput) {
     const li = criaLi();
     li.innerText = textoInput;
-    addTarefa.appendChild(li)
+    addTarefas.appendChild(li)
     limparInput();
-    criaBotaoApgar(li);
+    criaButtonApagar(li)
     saveTarefa();
-
 }
 
-function criaBotaoApgar(li){
+function criaButtonApagar(li) {
     li.innerText += ' ';
-    const btnDelet = document.createElement('button');
-    btnDelet.innerText = 'Apagar';
-    btnDelet.setAttribute('class', 'apagar');
-    li.appendChild(btnDelet);
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'apagar esta tarefa');
+    li.appendChild(botaoApagar);
 }
 
-document.addEventListener('click', function(e){
+
+inputBtn.addEventListener('click', function () {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener('click', function (e) {
     const el = e.target;
-    if (el.classList.contains('apagar')){
+    if (el.classList.contains('apagar')) {
         el.parentElement.remove()
-        saveTarefa();
+        saveTarefa()
     }
 
-})
+});
 
-function saveTarefa(){
-    const liTarefas = addTarefa.querySelectorAll('li');
+function saveTarefa() {
+    const liTarefas = addTarefas.querySelectorAll('li');
     const listaDeTarefas = [];
-    for (let tarefa of liTarefas){
+    for (let tarefa of liTarefas) {
         let tarefaTexto = tarefa.innerText;
         tarefaTexto = tarefaTexto.replace('Apagar', ' ').trim();
         listaDeTarefas.push(tarefaTexto);
     }
-    const tarefaJSON = JSON.stringify(listaDeTarefas);
-    localStorage.setItem('tarefas', tarefaJSON);
+
+    const tarefaJSON = JSON.stringify(listaDeTarefas); // aqui convertemos o objeto JS para objeto JSON
+    localStorage.setItem('tarefas', tarefaJSON); // valor usado para recuperar é o primeiro parametro STR
+    console.log(tarefaJSON);
 }
 
-function addTarefasSalvas(){
-    const tarefas = localStorage.getItem('tarefas');
+function AddTarefaSalvas() {  // local storage é global pois é uma propriedade do navegador
+    const tarefas = localStorage.getItem('tarefas'); // aqui convertemos o objeto JSON para objeto JS de volta
     const listaDeTarefas = JSON.parse(tarefas);
 
-    for (let tarefa of listaDeTarefas){
+    for (let tarefa of listaDeTarefas) {
         criaTarefa(tarefa)
     }
+
 }
-addTarefasSalvas();
-};
-
-listOfTaks();
-
+AddTarefaSalvas();
